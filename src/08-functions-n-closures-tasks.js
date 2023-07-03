1b3ad0802ce9c72edae7bc1cf4e14198bd2b850e
Viktar Brutsis <viktarbrutsis@gmail.com>
2023-07-03 17:03:36 +0300

    feat: some changes

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -46,8 +46,10 @@ function getComposition(f, g) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  return function level(x) {
+    return x ** exponent;
+  };
 }
 
 
